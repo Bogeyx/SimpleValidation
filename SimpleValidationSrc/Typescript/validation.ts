581df6096714f0de3fe36975b9d7ff5424bb3bd8
Max vT <mvt@edevax.de>
2017-07-31 16:17:47 +0200

    Prototype

--- SimpleValidationSrc/Typescript/validation.ts
+++ SimpleValidationSrc/Typescript/validation.ts
@@ -1,10 +1,47 @@
-﻿class Validation {
-    constructor() {
+﻿var sel = (selector: string) => document.querySelector(selector) as HTMLElement;
+var selAll = (selector: string) => document.querySelectorAll(selector) as NodeListOf<HTMLElement>;
 
+class Validation {
+    private _items: HTMLInputElement[];
+
+    constructor(items: NodeListOf<HTMLElement>) {
+        for (var i = 0; i < items.length; i++) {
+            this._items.push(items[i] as HTMLInputElement);
+        }
+    }
+
+    public validate(form: HTMLFormElement): boolean {
+        let message :string;
+
+        this._items.forEach(item => {
+            // Compare
+            if (item.dataset.equalto && item.dataset.equaltoOther) {
+                let other = sel('[name="' + item.dataset.equaltoOther + '"]') as HTMLInputElement;
+                if (item.value !== other.value) {
+                    message = item.dataset.equalto;
+                }
+            }
+
+            if (item.dataset.required) {
+                if (!item.value) {
+                    message = item.dataset.required;
+                }
+            }
+
+
+            if (message) {
+                item.setCustomValidity(message);
+            }
+        });
+
+        return !message;
     }
 }
 
 var validation: Validation;
 (function () {
-    validation = new Validation();
+    let valItems = selAll('[data-val="true"]');
+    if (valItems.length > 0) {
+        validation = new Validation(valItems);
+    }
 })();
--- dist/simpleValidation.js
+++ dist/simpleValidation.js
@@ -1,10 +1,39 @@
+var sel = function (selector) { return document.querySelector(selector); };
+var selAll = function (selector) { return document.querySelectorAll(selector); };
 var Validation = (function () {
-    function Validation() {
+    function Validation(items) {
+        for (var i = 0; i < items.length; i++) {
+            this._items.push(items[i]);
+        }
     }
+    Validation.prototype.validate = function (form) {
+        var message;
+        this._items.forEach(function (item) {
+            // Compare
+            if (item.dataset.equalto && item.dataset.equaltoOther) {
+                var other = sel('[name="' + item.dataset.equaltoOther + '"]');
+                if (item.value !== other.value) {
+                    message = item.dataset.equalto;
+                }
+            }
+            if (item.dataset.required) {
+                if (!item.value) {
+                    message = item.dataset.required;
+                }
+            }
+            if (message) {
+                item.setCustomValidity(message);
+            }
+        });
+        return !message;
+    };
     return Validation;
 }());
 var validation;
 (function () {
-    validation = new Validation();
+    var valItems = selAll('[data-val="true"]');
+    if (valItems.length > 0) {
+        validation = new Validation(valItems);
+    }
 })();
 //# sourceMappingURL=validation.js.map
