4c9c3328abbe1414926739cf258961e8f90e0302
Max vT <mvt@edevax.de>
2017-08-03 19:13:02 +0200

    Alle Jquery Validierungen implementiert

--- SimpleValidationSrc/Typescript/validation.js
+++ SimpleValidationSrc/Typescript/validation.js
@@ -1,5 +1,7 @@
+/// Shortcuts
 var sel = function (selector) { return document.querySelector(selector); };
 var selAll = function (selector) { return document.querySelectorAll(selector); };
+/// Das Validierungs-Plugin
 var Validation = (function () {
     function Validation() {
         var _this = this;
@@ -14,19 +16,15 @@ var Validation = (function () {
             _loop_1();
         }
     }
+    /// Validiert alle Elemente des Forms
     Validation.prototype.validate = function (form, e) {
         var valid = true;
-        try {
-            var items = form.querySelectorAll('[data-val="true"]:not([type="hidden"]');
-            for (var i = 0; i < items.length; i++) {
-                var item = items[i];
-                if (!(this.checkItem(item, form))) {
-                    valid = false;
-                }
+        var items = form.querySelectorAll('[data-val="true"]:not([type="hidden"]');
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (!(this.checkItem(item, form))) {
+                valid = false;
             }
-        }
-        catch (e) {
-            valid = false;
         }
         if (!valid) {
             e.preventDefault();
@@ -37,96 +35,128 @@ var Validation = (function () {
             return true;
         }
     };
+    /// Validiert ein Element des Forms
     Validation.prototype.checkItem = function (item, form) {
+        var _this = this;
         var message = "7";
-        var value = this.getUsefullValue(item, form);
-        // Required
-        if (item.dataset.valRequired) {
-            if (!value) {
-                message = item.dataset.valRequired;
+        try {
+            var value = this.getUsefullValue(item, form);
+            // Required
+            if (item.dataset.valRequired) {
+                if (!value) {
+                    message = item.dataset.valRequired;
+                }
+            }
+            if (value) {
+                // Compare
+                if (item.dataset.valEqualto && item.dataset.valEqualtoOther) {
+                    var other = sel('[name="' + item.dataset.equaltoOther + '"]');
+                    if (value !== this.getUsefullValue(other, form)) {
+                        message = item.dataset.valEqualto;
+                    }
+                }
+                // CreditCard
+                if (item.dataset.valCreditcard) {
+                    if (!(/^[0-9 \-]{10,}$/.test(value))) {
+                        message = item.dataset.valCreditcard;
+                    }
+                }
+                // Email
+                if (item.dataset.valEmail) {
+                    if (!(/^.+@.+\..+$/.test(value))) {
+                        message = item.dataset.valEmail;
+                    }
+                }
+                // Max
+                if (item.dataset.valMaxlength) {
+                    if (value.length > parseInt(item.dataset.valMaxlengthMax)) {
+                        message = item.dataset.valMaxlength;
+                    }
+                }
+                // Min
+                if (item.dataset.valMinlength) {
+                    if (value.length < parseInt(item.dataset.valMinlengthMin)) {
+                        message = item.dataset.valMinlength;
+                    }
+                }
+                // Range
+                if (item.dataset.valRange) {
+                    if (value.length < parseInt(item.dataset.valRangeMin)
+                        || value.length > parseInt(item.dataset.valRangeMax)) {
+                        message = item.dataset.valRange;
+                    }
+                }
+                // Regex
+                if (item.dataset.valRegex) {
+                    var reg = new RegExp(item.dataset.valRegexPattern);
+                    if (!reg.test(value)) {
+                        message = item.dataset.valRegex;
+                    }
+                }
+                // Length
+                if (item.dataset.valLength) {
+                    if (value.length !== parseInt(item.dataset.valLengthMax)) {
+                        message = item.dataset.valLength;
+                    }
+                }
+                // Password
+                if (item.dataset.valPassword) {
+                    var reg1 = /^[^%\s]{6,}$/;
+                    var reg2 = /[a-zA-Z]/;
+                    var reg3 = /[0-9]/;
+                    if (!(reg1.test(value) && reg2.test(value) && reg3.test(value))) {
+                        message = item.dataset.valPassword;
+                    }
+                }
+                // Date
+                if (item.dataset.valDate) {
+                    if (!(new Date(value))) {
+                        message = item.dataset.valDate;
+                    }
+                }
+                // Url
+                if (item.dataset.valUrl) {
+                    if (!(/^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/.test(value))) {
+                        message = item.dataset.valUrl;
+                    }
+                }
+                // Phone
+                if (item.dataset.valPhone) {
+                    if (!(/^(?=.*\d{3,})[0-9()\/\\ +-]{6,}$/.test(value))) {
+                        message = item.dataset.valPhone;
+                    }
+                }
+                // Digits 
+                if (item.dataset.valDigits) {
+                    if (!(/^[0-9]+$/.test(value))) {
+                        message = item.dataset.valDigits;
+                    }
+                }
+                // Numbers 
+                if (item.dataset.valNumber) {
+                    if (!(/^([0-9]{1,4}[.,]?)+$/.test(value))) {
+                        message = item.dataset.valNumber;
+                    }
+                }
             }
         }
-        if (value) {
-            // Compare
-            if (item.dataset.valEqualto && item.dataset.valEqualtoOther) {
-                var other = sel('[name="' + item.dataset.equaltoOther + '"]');
-                if (value !== this.getUsefullValue(other, form)) {
-                    message = item.dataset.valEqualto;
-                }
-            }
-            // CreditCard
-            if (item.dataset.valCreditcard) {
-                if (!(/^[0-9 \-]{10,}$/.test(value))) {
-                    message = item.dataset.valCreditcard;
-                }
-            }
-            // Email
-            if (item.dataset.valEmail) {
-                if (!(/^.+@.+\..+$/.test(value))) {
-                    message = item.dataset.valEmail;
-                }
-            }
-            // Max
-            if (item.dataset.valMaxlength) {
-                if (value.length > parseInt(item.dataset.valMaxlengthMax)) {
-                    message = item.dataset.valMaxlength;
-                }
-            }
-            // Min
-            if (item.dataset.valMinlength) {
-                if (value.length < parseInt(item.dataset.valMinlengthMin)) {
-                    message = item.dataset.valMinlength;
-                }
-            }
-            // Range
-            if (item.dataset.valRange) {
-                if (value.length < parseInt(item.dataset.valRangeMin)
-                    || value.length > parseInt(item.dataset.valRangeMax)) {
-                    message = item.dataset.valRange;
-                }
-            }
-            // Regex
-            if (item.dataset.valRegex) {
-                var reg = new RegExp(item.dataset.valRegexPattern);
-                if (!reg.test(value)) {
-                    message = item.dataset.valRegex;
-                }
-            }
-            // Length
-            if (item.dataset.valLength) {
-                if (value.length !== parseInt(item.dataset.valLengthMax)) {
-                    message = item.dataset.valLength;
-                }
-            }
-            // Password
-            if (item.dataset.valPassword) {
-                var reg1 = /^[^%\s]{6,}$/;
-                var reg2 = /[a-zA-Z]/;
-                var reg3 = /[0-9]/;
-                if (!(reg1.test(value) && reg2.test(value) && reg3.test(value))) {
-                    message = item.dataset.valPassword;
-                }
-            }
-            // Date
-            if (item.dataset.valLength) {
-            }
-            // Url
-            if (item.dataset.valLength) {
-            }
-            // digits 
-            if (item.dataset.valLength) {
-            }
-            // numbers 
-            if (item.dataset.valLength) {
-            }
-            // remote 
-            if (item.dataset.valLength) {
-            }
+        catch (e) {
+            message = "Diese Eingabe ist ungültig";
         }
         // Ergebnis anwenden
         var errorSpan = form.querySelector('[data-valmsg-for="' + item.name + '"');
         if (message !== "7") {
             item.setCustomValidity(message);
+            if (!item.onkeyup) {
+                item.onkeyup = function (e) {
+                    _this.checkItem(item, form);
+                };
+            }
+            if (!item.onchange) {
+                item.onchange = function (e) {
+                    _this.checkItem(item, form);
+                };
+            }
             if (errorSpan) {
                 errorSpan.classList.add("field-validation-error");
                 errorSpan.classList.remove("field-validation-valid");
@@ -149,7 +179,7 @@ var Validation = (function () {
             return item.checked;
         }
         else if (item.type === "radio") {
-            var radios = form.querySelectorAll('[type="radio][name="' + item.name + '"]');
+            var radios = form.querySelectorAll('[type="radio"][name="' + item.name + '"]');
             var checked = false;
             for (var i = 0; i < radios.length; i++) {
                 if (radios[i].checked) {
--- SimpleValidationSrc/Typescript/validation.ts
+++ SimpleValidationSrc/Typescript/validation.ts
@@ -1,6 +1,8 @@
-﻿var sel = (selector: string) => document.querySelector(selector) as HTMLElement;
+﻿/// Shortcuts
+var sel = (selector: string) => document.querySelector(selector) as HTMLElement;
 var selAll = (selector: string) => document.querySelectorAll(selector) as NodeListOf<HTMLElement>;
 
+/// Das Validierungs-Plugin
 class Validation {
     constructor() {
         let forms = selAll('form');
@@ -13,24 +15,17 @@ class Validation {
         }
     }
 
+    /// Validiert alle Elemente des Forms
     public validate(form: HTMLFormElement, e: Event): boolean {
         let valid = true;
+        let items = form.querySelectorAll('[data-val="true"]:not([type="hidden"]');
 
-        try {            
-            let items = form.querySelectorAll('[data-val="true"]:not([type="hidden"]');
-
-            for (var i = 0; i < items.length; i++) {                
-                let item = items[i] as HTMLInputElement;
-                if(!(this.checkItem(item, form))) {
-                    valid = false;
-                }
+        for (var i = 0; i < items.length; i++) {
+            let item = items[i] as HTMLInputElement;
+            if (!(this.checkItem(item, form))) {
+                valid = false;
             }
         }
-        catch (e)
-        {
-            valid = false;
-        }
-
 
         if (!valid) {
             e.preventDefault();
@@ -41,112 +36,128 @@ class Validation {
         }
     }
 
-
+    /// Validiert ein Element des Forms
     private checkItem(item: HTMLInputElement, form: HTMLFormElement) {
-        let message: string = "7";
-        let value = this.getUsefullValue(item, form);
+        let message: string = "7";  
+        
+        try {
+            let value = this.getUsefullValue(item, form);
 
-        // Required
-        if (item.dataset.valRequired) {
-            if (!value) {
-                message = item.dataset.valRequired;
+            // Required
+            if (item.dataset.valRequired) {
+                if (!value) {
+                    message = item.dataset.valRequired;
+                }
+            }
+
+            if (value) {
+                // Compare
+                if (item.dataset.valEqualto && item.dataset.valEqualtoOther) {
+                    let other = sel('[name="' + item.dataset.equaltoOther + '"]') as HTMLInputElement;
+                    if (value !== this.getUsefullValue(other, form)) {
+                        message = item.dataset.valEqualto;
+                    }
+                }
+
+                // CreditCard
+                if (item.dataset.valCreditcard) {
+                    if (!(/^[0-9 \-]{10,}$/.test(value))) {
+                        message = item.dataset.valCreditcard;
+                    }
+                }
+
+                // Email
+                if (item.dataset.valEmail) {
+                    if (!(/^.+@.+\..+$/.test(value))) {
+                        message = item.dataset.valEmail;
+                    }
+                }
+
+                // Max
+                if (item.dataset.valMaxlength) {
+                    if ((value as string).length > parseInt(item.dataset.valMaxlengthMax)) {
+                        message = item.dataset.valMaxlength;
+                    }
+                }
+
+                // Min
+                if (item.dataset.valMinlength) {
+                    if ((value as string).length < parseInt(item.dataset.valMinlengthMin)) {
+                        message = item.dataset.valMinlength;
+                    }
+                }
+
+                // Range
+                if (item.dataset.valRange) {
+                    if ((value as string).length < parseInt(item.dataset.valRangeMin)
+                        || (value as string).length > parseInt(item.dataset.valRangeMax)) {
+                        message = item.dataset.valRange;
+                    }
+                }
+
+                // Regex
+                if (item.dataset.valRegex) {
+                    var reg = new RegExp(item.dataset.valRegexPattern);
+                    if (!reg.test(value)) {
+                        message = item.dataset.valRegex;
+                    }
+                }
+
+                // Length
+                if (item.dataset.valLength) {
+                    if ((value as string).length !== parseInt(item.dataset.valLengthMax)) {
+                        message = item.dataset.valLength;
+                    }
+                }
+
+                // Password
+                if (item.dataset.valPassword) {
+                    var reg1 = /^[^%\s]{6,}$/;
+                    var reg2 = /[a-zA-Z]/;
+                    var reg3 = /[0-9]/;
+                    if (!(reg1.test(value) && reg2.test(value) && reg3.test(value))) {
+                        message = item.dataset.valPassword;
+                    }
+                }
+
+                // Date
+                if (item.dataset.valDate) {
+                    if (!(new Date(value))) {
+                        message = item.dataset.valDate;
+                    }
+                }
+
+                // Url
+                if (item.dataset.valUrl) {
+                    if (!(/^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/.test(value))) {
+                        message = item.dataset.valUrl;
+                    }
+                }
+
+                // Phone
+                if (item.dataset.valPhone) {
+                    if (!(/^(?=.*\d{3,})[0-9()\/\\ +-]{6,}$/.test(value))) {
+                        message = item.dataset.valPhone;
+                    }
+                }
+
+                // Digits 
+                if (item.dataset.valDigits) {
+                    if (!(/^[0-9]+$/.test(value))) {
+                        message = item.dataset.valDigits;
+                    }
+                }
+
+                // Numbers 
+                if (item.dataset.valNumber) {
+                    if (!(/^([0-9]{1,4}[.,]?)+$/.test(value))) {
+                        message = item.dataset.valNumber;
+                    }
+                }
             }
         }
-
-        if (value) {
-            // Compare
-            if (item.dataset.valEqualto && item.dataset.valEqualtoOther) {
-                let other = sel('[name="' + item.dataset.equaltoOther + '"]') as HTMLInputElement;
-                if (value !== this.getUsefullValue(other, form)) {
-                    message = item.dataset.valEqualto;
-                }
-            }
-
-            // CreditCard
-            if (item.dataset.valCreditcard) {
-                if (!(/^[0-9 \-]{10,}$/.test(value))) {
-                    message = item.dataset.valCreditcard;
-                }
-            }
-
-            // Email
-            if (item.dataset.valEmail) {
-                if (!(/^.+@.+\..+$/.test(value))) {
-                    message = item.dataset.valEmail;
-                }
-            }
-
-            // Max
-            if (item.dataset.valMaxlength) {
-                if ((value as string).length > parseInt(item.dataset.valMaxlengthMax)) {
-                    message = item.dataset.valMaxlength;
-                }
-            }
-
-            // Min
-            if (item.dataset.valMinlength) {
-                if ((value as string).length < parseInt(item.dataset.valMinlengthMin)) {
-                    message = item.dataset.valMinlength;
-                }
-            }
-
-            // Range
-            if (item.dataset.valRange) {
-                if ((value as string).length < parseInt(item.dataset.valRangeMin)
-                    || (value as string).length > parseInt(item.dataset.valRangeMax)) {
-                    message = item.dataset.valRange;
-                }
-            }
-
-            // Regex
-            if (item.dataset.valRegex) {
-                var reg = new RegExp(item.dataset.valRegexPattern);
-                if (!reg.test(value)) {
-                    message = item.dataset.valRegex;
-                }
-            }
-
-            // Length
-            if (item.dataset.valLength) {
-                if ((value as string).length !== parseInt(item.dataset.valLengthMax)) {
-                    message = item.dataset.valLength;
-                }
-            }
-
-            // Password
-            if (item.dataset.valPassword) {
-                var reg1 = /^[^%\s]{6,}$/;
-                var reg2 = /[a-zA-Z]/;
-                var reg3 = /[0-9]/;
-                if (!(reg1.test(value) && reg2.test(value) && reg3.test(value))) {
-                    message = item.dataset.valPassword;
-                }
-            }
-
-            // Date
-            if (item.dataset.valLength) {
-
-            }
-
-            // Url
-            if (item.dataset.valLength) {
-
-            }
-
-            // digits 
-            if (item.dataset.valLength) {
-
-            }
-
-            // numbers 
-            if (item.dataset.valLength) {
-
-            }
-
-            // remote 
-            if (item.dataset.valLength) {
-
-            }
+        catch (e) {
+            message = "Diese Eingabe ist ungültig";
         }
 
 
@@ -154,6 +165,17 @@ class Validation {
         let errorSpan = form.querySelector('[data-valmsg-for="' + item.name + '"') as HTMLSpanElement;
         if (message !== "7") {
             item.setCustomValidity(message);
+            if (!item.onkeyup) {
+                item.onkeyup = (e) => {
+                    this.checkItem(item, form)
+                }
+            }
+
+            if (!item.onchange) {
+                item.onchange = (e) => {
+                    this.checkItem(item, form)
+                }
+            }
 
             if (errorSpan) {
                 errorSpan.classList.add("field-validation-error");
@@ -179,7 +201,7 @@ class Validation {
         if (item.type === "checkbox") {
             return item.checked;
         } else if (item.type === "radio") {
-            let radios = form.querySelectorAll('[type="radio][name="' + item.name + '"]') as NodeListOf<HTMLInputElement>;
+            let radios = form.querySelectorAll('[type="radio"][name="' + item.name + '"]') as NodeListOf<HTMLInputElement>;
             let checked = false;
             for (var i = 0; i < radios.length; i++) {
                 if (radios[i].checked) {
